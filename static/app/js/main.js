6cc491945ae822453420892b5f82b335cce98e72
oyyq99999 <oyyq9999999@163.com>
2016-06-28 00:35:48 +1200

    fix some minor bugs

--- static/app/js/main.js
+++ static/app/js/main.js
@@ -11,8 +11,10 @@ $(window).on('hashchange', function() {
 	var countryId = window.location.hash.substr(1);
 	var countryHash = $('[name="' + countryId + '"]');
 	$('tr').removeClass('warning');
-	$('html, body').animate({
-		scrollTop: countryHash.offset().top
-	}, 200);
+	if (countryHash.size() > 0) {
+		$('html, body').animate({
+			scrollTop: countryHash.offset().top
+		}, 200);
+	}
 	countryHash.parents('tr').addClass('warning');
 }).trigger('hashchange');
